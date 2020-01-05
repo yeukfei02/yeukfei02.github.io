f14d2aad1af711f85f9235fed7f167d17f167dae
Donald Wu <yeukfei02@gmail.com>
2020-01-05 22:25:53 +0800

    fix-code: copy build folder outside

---
+++ precache-manifest.b0cd2b1cbbd14ab1af5ef001f36ef383.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9322dad119b12b7664c3e74d5d4fac9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5821ceddc1e3cd3ef6c4",
+    "url": "/static/css/main.3a566663.chunk.css"
+  },
+  {
+    "revision": "2d1d2260df2e7f2b7097",
+    "url": "/static/js/2.0290099e.chunk.js"
+  },
+  {
+    "revision": "9652715b819d26dcc09aad3261a56a36",
+    "url": "/static/js/2.0290099e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5821ceddc1e3cd3ef6c4",
+    "url": "/static/js/main.497d52ea.chunk.js"
+  },
+  {
+    "revision": "9aedf1ef7999bbd8dbd5",
+    "url": "/static/js/runtime-main.01ad01f3.js"
+  },
+  {
+    "revision": "5e6b14804709cbedbe3049a81e7c2159",
+    "url": "/static/media/profile.5e6b1480.png"
+  }
+]);

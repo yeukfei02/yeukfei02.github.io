5860ca5b6e7b0ffebc08270b6de9abf2b124ef3e
Donald Wu <yeukfei02@gmail.com>
2020-05-02 13:42:38 +0800

    fix-code: move build folder to outside

---
+++ precache-manifest.ac7c914e920707f1acbaf6d6a521b1cb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce75a6246c19827a78862a3433dbe2b0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "18ee68d7003241d120a2",
+    "url": "/static/css/main.9089fa3a.chunk.css"
+  },
+  {
+    "revision": "2d1d2260df2e7f2b7097",
+    "url": "/static/js/2.0290099e.chunk.js"
+  },
+  {
+    "revision": "9652715b819d26dcc09aad3261a56a36",
+    "url": "/static/js/2.0290099e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "18ee68d7003241d120a2",
+    "url": "/static/js/main.8c6156f9.chunk.js"
+  },
+  {
+    "revision": "9aedf1ef7999bbd8dbd5",
+    "url": "/static/js/runtime-main.01ad01f3.js"
+  },
+  {
+    "revision": "5e6b14804709cbedbe3049a81e7c2159",
+    "url": "/static/media/profile.5e6b1480.png"
+  }
+]);

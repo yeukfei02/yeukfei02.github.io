5c6931615b7a24dd00320e23fd230e3d020769c2
Donald Wu <yeukfei02@gmail.com>
2022-03-31 21:38:26 +0800

    fix: update build folder files

---
+++ precache-manifest.39ea2702d70f1e7b09d38838f57d222c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d8f6db1795641ee4fdf59becb1d8bd8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "37b95d2d8e3bf9acef4b",
+    "url": "/static/css/main.df1fb7fe.chunk.css"
+  },
+  {
+    "revision": "5e672924110e861769ae",
+    "url": "/static/js/2.4e32a76d.chunk.js"
+  },
+  {
+    "revision": "ab2e3b77e74dc35161f45410c415e5b4",
+    "url": "/static/js/2.4e32a76d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "37b95d2d8e3bf9acef4b",
+    "url": "/static/js/main.120bca10.chunk.js"
+  },
+  {
+    "revision": "9aedf1ef7999bbd8dbd5",
+    "url": "/static/js/runtime-main.01ad01f3.js"
+  },
+  {
+    "revision": "5e6b14804709cbedbe3049a81e7c2159",
+    "url": "/static/media/profile.5e6b1480.png"
+  }
+]);
